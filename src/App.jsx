73bec96c7dbdfee3c8cf173c7d9772d2f59423cf
damian <the.dmian@live.cl>
2023-05-18 23:11:16 -0400

    ADD context for filters products

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
-import { useState } from 'react'
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header'
+import Footer from './components/Footer'
+import { IS_DEVELOPMENT } from './config'
+import { useFilters } from './hooks/useFilters'
 
 function App () {
-  const [products] = useState(initialProducts)
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
+  const { filterProducts } = useFilters()
 
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-  const filteredProducts = filterProducts(products)
+  const filteredProducts = filterProducts(initialProducts)
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer />}
     </>
   )
 }
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,20 +1,20 @@
-import { useState, useId } from 'react'
+import { useId } from 'react'
 import './Filters.css'
+import { useFilters } from '../hooks/useFilters'
 
-export function Filters ({ changeFilters }) {
-  const [minPrice, setMinPrice] = useState(0)
+export function Filters () {
+  const { filters, setFilters } = useFilters()
   const minPriceFilterId = useId()
   const categoryFilterId = useId()
 
   const handleChangeMinPrice = (e) => {
-    setMinPrice(e.target.value)
-    changeFilters(prevState => ({
+    setFilters(prevState => ({
       ...prevState,
       minPrice: e.target.value
     }))
   }
   const handleChangeCategory = (e) => {
-    changeFilters(prevState => ({
+    setFilters(prevState => ({
       ...prevState,
       category: e.target.value
     }))
@@ -29,8 +29,9 @@ export function Filters ({ changeFilters }) {
           min='0'
           max='1000'
           onChange={handleChangeMinPrice}
+          value={filters.minPrice}
         />
-        <span>$ {minPrice}</span>
+        <span>$ {filters.minPrice}</span>
       </div>
       <div>
         <label htmlFor={categoryFilterId}>Categoria</label>
