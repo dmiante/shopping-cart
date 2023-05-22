f987731712345b8a1f8a2f185db0c9f26b2ef982
damian <the.dmian@live.cl>
2023-05-22 17:34:23 -0400

    ADD cart with context and reducer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,9 @@
-import { useFilters } from '../hooks/useFilters'
 import './Footer.css'
 
 export default function Footer () {
-  const { filters } = useFilters()
   return (
     <footer className='footer'>
-      {
-        JSON.stringify(filters, null, 2)
-      }
+      <p>Shopping Cart Practice - Dmiante 🦁</p>
     </footer>
   )
 }
